d69c140a1d4e434906291c662b7c683697ebf858
RAES9 <erivasdeveloper@gmail.com>
2021-03-01 17:22:51 -0600

    Delete inecesary code

--- src/com/raes/Main.java
+++ src/com/raes/Main.java
@@ -1,11 +1,8 @@
 package com.raes;
-import javax.swing.table.DefaultTableModel;
-import java.util.Scanner;
 
 public class Main {
     static Queue queue = new Queue();
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
         GetExchange get = new GetExchange();
         for (Currency.select currency : Currency.select.values()) {
             if (!(currency.toString().equals("USD"))) {
